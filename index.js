9875ae9fd3ec1a819666fe375c0e55b75152e07c
bread-Cat13 <jaebaek1101@gmail.com>
2024-08-21 20:01:37 +0900

    Add cloned files and directory structure

--- index.js
+++
@@ -1,4 +0,0 @@
-// scripts.js
-document.addEventListener("DOMContentLoaded", function() {
-    // 이후 추가적인 인터랙션을 위한 코드 작성 가능
-});
---
+++ start/index.js
@@ -0,0 +1,4 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    // 이후 추가적인 인터랙션을 위한 코드 작성 가능
+});
