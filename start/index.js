e785e916143a388a9779d99ecea038c42cb20c88
pupusa370 <83807403+pupusa370@users.noreply.github.com>
2024-08-22 11:42:03 +0900

    Update index.js

--- start/index.js
+++ start/index.js
@@ -1,4 +1,4 @@
 // scripts.js
-document.addEventListener("DOMContentLoaded", function() {
-    // 이후 추가적인 인터랙션을 위한 코드 작성 가능
-});
+document.addEventListener("click", function() {
+    window.location.href = "select_region.html";
+})
