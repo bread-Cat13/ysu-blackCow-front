194ec2cbac8a9281034150974c8a457da2852052
pupusa370 <83807403+pupusa370@users.noreply.github.com>
2024-08-24 12:35:16 +0900

    Update select_region.js

--- region/select_region.js
+++ region/select_region.js
@@ -2,17 +2,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // 송도캠퍼스 선택 시
     document.getElementById("right-selection").addEventListener("click", function() {
-        window.location.href = "songdo.html"; // 송도 캠퍼스 페이지로 이동
+        window.location.href = "map/songdo.html"; // 송도 캠퍼스 페이지로 이동
     });
 
     // 신촌캠퍼스 선택 시
     document.getElementById("left-selection").addEventListener("click", function() {
-        window.location.href = "sinchon.html"; // 신촌 캠퍼스 페이지로 이동
-    });
-
-    // 언어모델 아이콘 클릭 시 (다음 페이지로 이동 예정)
-    document.getElementById("chat-icon").addEventListener("click", function() {
-        // 이후 3페이지에서 설명할 기능 추가 예정
+        window.location.href = "map/sinchon.html"; // 신촌 캠퍼스 페이지로 이동
     });
 });
 
